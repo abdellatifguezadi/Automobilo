1f75ee5d6802e9a43722d5c36a389ad4d5fd7c6f
vm3do <medoavvadi@gmail.com>
2026-02-01 20:02:59 +0100

    fix: remove the deconnexion button from sidebar & update buttons style in the header

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,10 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Marque } from '../../models/car.model';
-import { selectIsAuthenticated } from '../../store/auth/auth.selectors';
 import * as CarSelectors from '../../store/cars/car.selectors';
 import * as CarActions from '../../store/cars/car.actions';
-import * as AuthActions from '../../store/auth/auth.actions';
 
 @Component({
   selector: 'app-sidebar',
@@ -19,22 +17,14 @@ export class SidebarComponent {
   marques$: Observable<Marque[]>;
   selectedMarque$: Observable<number | null>;
   showAvailableOnly$: Observable<boolean>;
-  isAuthenticated$: Observable<boolean>;
 
   constructor(private store: Store) {
     this.marques$ = this.store.select(CarSelectors.selectMarques);
     this.selectedMarque$ = this.store.select(CarSelectors.selectSelectedMarque);
     this.showAvailableOnly$ = this.store.select(CarSelectors.selectShowAvailableOnly);
-    this.isAuthenticated$ = this.store.select(selectIsAuthenticated);
   }
 
   onMarqueClick(marqueId: number | null) {
     this.store.dispatch(CarActions.setMarqueFilter({ marqueId }));
-  }
-
-
-
-  onLogout() {
-    this.store.dispatch(AuthActions.logout());
   }
 }
